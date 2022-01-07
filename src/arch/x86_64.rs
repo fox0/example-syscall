332b438db3800cc8b78acb527660e95604cc54b4
fox0 <red-fox0@yandex.ru>
2022-01-07 15:15:39 +0700

    +enum

--- src/arch/x86_64.rs
+++ src/arch/x86_64.rs
@@ -3,39 +3,55 @@ use std::arch::asm;
 
 pub const STDOUT: usize = 1;
 
-//todo enum
-const SYS_WRITE: u64 = 1;
-const SYS_EXIT: u64 = 60;
+#[derive(Copy, Clone)]
+enum Syscalls {
+    Write = 1,
+    Exit = 60,
+}
 
+#[cfg(target_pointer_width = "64")]
 #[must_use]
 #[allow(clippy::cast_possible_truncation)]
 pub fn sys_write(fd: usize, buf: &str) -> usize {
-    let ret = unsafe { syscall3(SYS_WRITE, fd as u64, buf.as_ptr() as u64, buf.len() as u64) };
+    let ret = unsafe {
+        syscall3(
+            Syscalls::Write,
+            fd as u64,
+            buf.as_ptr() as u64,
+            buf.len() as u64,
+        )
+    };
     ret as usize
 }
 
 #[allow(clippy::empty_loop)]
 pub fn sys_exit(error_code: u8) -> ! {
-    let _ = unsafe { syscall1(SYS_EXIT, u64::from(error_code)) };
+    let _ = unsafe { syscall1(Syscalls::Exit, error_code) };
     loop {}
 }
 
-unsafe fn syscall1(name: u64, arg1: u64) -> u64 {
+unsafe fn syscall1<T: Into<u64>>(name: Syscalls, arg1: T) -> u64 {
+    let arg0 = name as u64;
+    let arg1 = arg1.into();
     let ret;
     asm!(
         "syscall",
-        in("rax") name,
+        in("rax") arg0,
         in("rdi") arg1,
         lateout("rax") ret,
     );
     ret
 }
 
-unsafe fn syscall3(name: u64, arg1: u64, arg2: u64, arg3: u64) -> u64 {
+unsafe fn syscall3<T: Into<u64>>(name: Syscalls, arg1: T, arg2: T, arg3: T) -> u64 {
+    let arg0 = name as u64;
+    let arg1 = arg1.into();
+    let arg2 = arg2.into();
+    let arg3 = arg3.into();
     let ret;
     asm!(
         "syscall",
-        in("rax") name,
+        in("rax") arg0,
         in("rdi") arg1,
         in("rsi") arg2,
         in("rdx") arg3,
